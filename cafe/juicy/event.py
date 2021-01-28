5367b6ad355d66b46420d6893ff3b06c507cbfca
Park18 <sowoomi35@gmai.com>
2021-01-28 22:05:35 +0900

    edit cafe.juicy.event
    edit comment

--- cafe/juicy/event.py
+++ cafe/juicy/event.py
@@ -1,12 +1,23 @@
-# 쥬씨 크롤링
-# 이벤트
-#   - 이벤트 세부 페이지 url
-#   - 이미지
-#   - 제목
+###############################################################
+# 이벤트 처리 클래스
+###############################################################
+#
+# 이벤트 추출 리스트
+#   - 이벤트 url
+#   - 이벤트 이미지
+#   - 이벤트 제목
 #   - 등록일
-# 이벤트 게시판 2페이지: "http://www.no1juicy.com/bbs/board/lists/2?bo_table=event"
-# 이벤트 상세: http://www.no1juicy.com/bbs/board/view?bo_table=event&wr_id=82
-# 이벤트 사진: http://www.no1juicy.com/uploads/board/event/82/thumb/cedcfaf0213b23e792b6d1d49bf90d23.png
+#
+# 이벤트 크롤링 관련 정보
+#   - 이벤트 게시판 1페이지: http://www.no1juicy.com/bbs/board/lists/?bo_table=event
+#   - 이벤트 게시판 2페이지: http://www.no1juicy.com/bbs/board/lists/2?bo_table=event
+#   - 이벤트 url: http://www.no1juicy.com/bbs/board/view?bo_table=event&wr_id=82
+#   - 이벤트 사진: http://www.no1juicy.com/uploads/board/event/82/thumb/cedcfaf0213b23e792b6d1d49bf90d23.png
+#
+# TODO
+#   - DB 선택 및 추출한 데이터 DB에 넣는 처리
+#   - 추출한 이미지를 저장해야 하는가 이미지 url을 이용해야 하는가 
+###############################################################
 
 import os # 실행 위치
 
@@ -25,32 +36,34 @@ class Event:
         self.soup = BeautifulSoup(self.response, 'html.parser')
 
     def crawling(self):
-        #self.url()
+        self.url()
         self.image()
-        #self.title()
-        #self.date()
-
-        # TODO: 이후 공지 등이 추가 되었을 때 넣음
+        self.title()
+        self.date()
 
     def url(self):
         for anchor  in self.soup.select("ul.ntcList > li > a"):
             # 테스트
             print(BASE + anchor.get("href"))
 
-            # TODO: DB에 넣는 처리
+            # TODO: DB push
 
     def image(self):
         for anchor in self.soup.select("ul.ntcList > li > a > img"):
             img_url = BASE + anchor.get("src")
             img_name = img_url[img_url.rfind('/')+1:]
 
-            #print(EVENT_DIRECTORY)
+            # 이미지를 다운 받거나 img_url Db push
             urllib.request.urlretrieve(img_url, EVENT_DIRECTORY + img_name)
 
     def title(self):
         for anchor in self.soup.select("ul.ntcList > li > a > dl > dt"):
             print(anchor.get_text())
 
+            # TODO: DB push
+
     def date(self):
         for anchor in self.soup.select("ul.ntcList > li > a > dl > dd:nth-child(3)"):
             print(anchor.get_text())
+
+            # TODO: DB push
